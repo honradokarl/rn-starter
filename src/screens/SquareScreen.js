e3a2d4113f4154b76504fb823371141c11f025f5
Karl John Honrado <honradokarljohn@gmail.com>
2021-08-18 02:54:37 +0800

    59: restoring validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,24 +6,31 @@ const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
     // state === { red: number, green: number, blue: number };
-    // action === { colorToChange: 'red' || 'green' || 'blue' }
+    // action === { type: 'change_red' || 'change_green' || 'change_blue' }
 
     switch (action.colorToChange)
     {
         case 'red':
-            return { ...state, red: state.red + action.amount };
+            return state.red + action.amount > 255 || state.red + action.amount < 0
+                    ? state
+                    : { ...state, red: state.red + action.amount };
         case 'green':
-            return { ...state, green: state.green + action.amount };
+            return state.green + action.amount > 255 || state.green + action.amount < 0
+                    ? state
+                    :  { ...state, green: state.green + action.amount };
         case 'blue':
-            return { ...state, blue: state.blue + action.amount };
+            return state.blue + action.amount > 255 || state.blue + action.amount < 0
+                    ? state
+                    : { ...state, blue: state.blue + action.amount };
         default:
+            return state;
     }
 
 };
 
 const SquareScreen = () => {
 
-    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0 });
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
     const { red, green, blue } = state;
 
     return <View>
