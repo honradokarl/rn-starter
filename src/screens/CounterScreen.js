99e80e6752b1fcddbe79b78a4e6b55a1530ddd56
Karl John Honrado <honradokarljohn@gmail.com>
2021-08-18 03:29:08 +0800

    :hammer: 62: Exercise Solution: refactor CounterScreen.js

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,29 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from "react-native";
+
+const reducer = (state, action) => {
+    if (action.type === 'increase') {
+        return { ...state, counter: state.counter + 1 }
+    } else if (action.type === 'decrease') {
+        return { ...state, counter: state.counter - 1 }
+    }
+}
 
 const CounterScreen = () => {
     // todo: fix this
     // this is what we called an array of destructuring
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    const { counter } = state;
 
     return <View>
-        <Button title="Increase" onPress={() => {
-            setCounter(counter + 1);
-        }}/>
-        <Button title="Decrease" onPress={() => {
-            setCounter(counter - 1);
-        }}/>
+        <Button
+            title="Increase"
+            onPress={() => dispatch({ type: 'increase' })}
+        />
+        <Button
+            title="Decrease"
+            onPress={() => dispatch({ type: 'decrease' })}
+        />
         <Text>Current count: { counter } </Text>
     </View>
 }
